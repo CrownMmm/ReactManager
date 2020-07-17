7dbb566b601262279a454fd8069e77443b1d062a
zhangjiaming <823600537@qq.com>
2020-07-18 02:02:43 +0800

    feat:头部组件开发

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,41 +1,65 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'antd';
-import Util from '../../util/utils'
+import Util from '../../util/utils';
+import axios from '../../axios'
 import './index.less'
 class Header extends Component {
-  componentWillMount() {
-    this.setState({
-      userName: '铭'
-    })
-    setInterval(() => {
-      let sysTime = Util.formateDate(new Date().getTime());
-      this.setState({
-        sysTime
-      })
-    }, 1000)
-  }
-  render() {
-    return (
-      <div className="header">
-        <Row className="header-top">
-          <Col span="24">
-            <span>欢迎，{this.state.userName}</span>
-            <a href="#">退出</a>
-          </Col>
-        </Row>
-        <Row className="breadcrumb">
-          <Col span="4" className="breadcrumb-title">
-            首页
-          </Col>
-          <Col span="20" className="weather">
-            <span className="date">{this.state.sysTime}</span>
-            <span className="weather-detail">多云</span>
+    componentWillMount() {
+        this.setState({
+            userName: '铭'
+        })
+        setInterval(() => {
+            let sysTime = Util.formateDate(new Date().getTime());
+            this.setState({
+                sysTime
+            })
+        }, 1000)
+        this.getWeatherAPIData()
+    }
+    //   url: 'http://api.map.baidu.com/weather/v1/?district_id=222405&data_type=all&ak=GuUPsGY35M4miCo3aVlGPW8qFttnMFoI'
+    // url: 'http://api.map.baidu.com/telematics/v3/weather?location=' + encodeURIComponent(city) + '&output=json&ak=GuUPsGY35M4miCo3aVlGPW8qFttnMFoI'
+    //http://api.map.baidu.com/telematics/v3/weather?location=beijing&output=json&ak=3p49MVra6urFRGOT9s8UBWr2
 
+    getWeatherAPIData() {
+        let city = '杭州'
+        axios.jsonp({
+            url: 'http://api.map.baidu.com/telematics/v3/weather?location=' + encodeURIComponent(city) + '&output=json&ak=3p49MVra6urFRGOT9s8UBWr2'
+        }).then((res) => {
+            if (res.status) {
+                let data = res.results[0].weather_data[0];
+                this.setState({
+                    dayPictureUrl: data.dayPictureUrl,
+                    weather: data.weather
+                })
+            }
+        })
+    }
+    render() {
+        return (
+            <div className="header">
+                <Row className="header-top">
+                    <Col span="24">
+                        <span>欢迎，{this.state.userName}</span>
+                        <a href="#">退出</a>
+                    </Col>
+                </Row>
+                <Row className="breadcrumb">
+                    <Col span="4" className="breadcrumb-title">
+                        首页
           </Col>
-        </Row>
-      </div>
-    );
-  }
+                    <Col span="20" className="weather">
+                        <span className="date">{this.state.sysTime}</span>
+                        <span className="weather-detail">
+                            <img src={this.state.dayPictureUrl} />
+                        </span>
+                        <span className="weather-detail">
+                            {this.state.weather}
+                        </span>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
 }
 
 export default Header;
