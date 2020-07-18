ebfcd4a54b538cd7b605fd235efe97c82107a8a6
zhangjiaming <823600537@qq.com>
2020-07-19 02:00:51 +0800

    feat:测试路由

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 // import Life from './pages/demo/Life';
 import * as serviceWorker from './serviceWorker';
 import Admin from './admin.js';
+// import Home from './pages/route_demo/route1/Home';
+import Router from './pages/route_demo/route2/router';
+
 
 
 ReactDOM.render(
-  <Admin/>,
+  <Router/>,
   document.getElementById('root')
 );
 
