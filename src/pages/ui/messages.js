bc8ad074b973478b82fe9ed7f0a8cfe97d3e37b2
zhangjiaming <823600537@qq.com>
2020-07-24 00:51:28 +0800

    feat:message组件完成

---
+++ src/pages/ui/messages.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Card, Button, message } from 'antd';
+
+class Messages extends Component {
+
+    showMessage = (type) => {
+        message[type]("恭喜你",)
+    }
+
+    render() {
+        return (
+            <div>
+                <Card title="全局提示框" className="card-wrap">
+                    <Button type="primary" onClick={() => this.showMessage('success')}>Success</Button>
+                    <Button type="primary" onClick={() => this.showMessage('info')}>Info</Button>
+                    <Button type="primary" onClick={() => this.showMessage('warning')}>Waring</Button>
+                    <Button type="primary" onClick={() => this.showMessage('error')}>Error</Button>
+                    <Button type="primary" onClick={() => this.showMessage('loading')}>Loading</Button>
+                </Card>
+            </div>
+        );
+    }
+}
+
+export default Messages;
--- src/router.js
+++ src/router.js
@@ -7,6 +7,7 @@ import Buttons from './pages/ui/button'
 import Modals from './pages/ui/modals'
 import Loadings from './pages/ui/loading'
 import Notice from './pages/ui/notice'
+import Messages from './pages/ui/messages'
 
 import NoMatch from './pages/nomatch'
 class Router extends Component {
@@ -22,6 +23,7 @@ class Router extends Component {
                                 <Route path="/admin/ui/modals" component={Modals}></Route>
                                 <Route path="/admin/ui/loadings" component={Loadings}></Route>
                                 <Route path="/admin/ui/notification" component={Notice}></Route>
+                                <Route path="/admin/ui/messages" component={Messages}></Route>
 
                                 <Route component={NoMatch}></Route>
                             </Switch>
